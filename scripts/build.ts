01bb3cbff10ca1f0a2c35dce8d4cdcb86d47f11c
rmst <rmst@qq.com>
2025-05-17 22:11:45 +0800

    test

---
+++ scripts/build.ts
@@ -0,0 +1,11 @@
+import { execSync } from 'child_process'
+
+execSync('pnpm run build', {
+  stdio: 'inherit',
+  env: {
+    ...process.env,
+    TAURI_SIGNING_PRIVATE_KEY:
+      'dW50cnVzdGVkIGNvbW1lbnQ6IHJzaWduIGVuY3J5cHRlZCBzZWNyZXQga2V5ClJXUlRZMEl5SCtsVVZzZVpzcUtHZWJqOEh6Uy9BclZBM0VhWDd1QWVlMVY0Sjk4TmtFWUFBQkFBQUFBQUFBQUFBQUlBQUFBQXdoOElHYkI5UXJ1YjF3bEZ6aVNBTUovcmRrOStJSEdpd24zQkgza3dXejU2NHdLTHczT2VGZ2JVVURHQjJocE5SMkNMYlFnV1JqUVZhMGw3UElPeERTd2M0b21XclIvSWR0WHJBUUlPT2N6cHpOcmtRYnBFM2VpYWowTUxXak9xcXJveEl2bDQzdGM9Cg==',
+    TAURI_SIGNING_PRIVATE_KEY_PASSWORD: 'rmst.4480'
+  }
+})
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -33,9 +33,9 @@ pub fn run() {
     .plugin(tauri_plugin_dialog::init())
     .plugin(tauri_plugin_clipboard_manager::init())
     .plugin(tauri_plugin_store::Builder::new().build())
-    .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {
-      let _ = show_window(app);
-    }))
+    // .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {
+    //   // let _ = show_window(app);
+    // }))
     .plugin(tauri_plugin_store::Builder::default().build())
     .plugin(tauri_plugin_opener::init())
     .invoke_handler(tauri::generate_handler![
