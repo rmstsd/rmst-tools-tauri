edd3cc5e6144e3d23c338ad4c53d11a235a0142c
李春雷 <rmstsd@qq.com>
2025-05-19 18:14:52 +0800

    qrcode

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -1,10 +1,11 @@
 import { Tabs } from '@arco-design/web-react'
+import { useEffect, useState } from 'react'
 import SettingConfig from './SettingConfig'
 import SmallTool from './SmallTool'
-import { useEffect, useState } from 'react'
+import QrCode from './QrCode'
 
 export default function SettingPage() {
-  const [activeKey, setActiveKey] = useState('1')
+  const [activeKey, setActiveKey] = useState(localStorage.getItem('activeKey') || '1')
   const [qrCodeValue, setQrCodeValue] = useState('rmst')
 
   useEffect(() => {}, [])
@@ -13,7 +14,10 @@ export default function SettingPage() {
     <Tabs
       tabPosition="left"
       activeTab={activeKey}
-      onChange={setActiveKey}
+      onChange={k => {
+        localStorage.setItem('activeKey', k)
+        setActiveKey(k)
+      }}
       destroyOnHide
       className="setting-page-root h-screen"
     >
@@ -23,9 +27,9 @@ export default function SettingPage() {
       <Tabs.TabPane key="2" title="工具" className="h-screen overflow-auto">
         <SmallTool />
       </Tabs.TabPane>
-      {/* <Tabs.TabPane key="3" title="二维码" className="h-screen overflow-auto">
+      <Tabs.TabPane key="3" title="二维码" className="h-screen overflow-auto">
         <QrCode value={qrCodeValue} setValue={setQrCodeValue} />
-      </Tabs.TabPane> */}
+      </Tabs.TabPane>
     </Tabs>
   )
 }
