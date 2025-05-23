d7e73e3de1eebc1ba756ba994166a70d19376a5f
rmstsd <rmstsd@qq.com>
2025-05-23 13:14:52 +0800

    o

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -22,6 +22,86 @@ pub fn run() {
   let builder = tauri::Builder::default();
 
   builder
+    .plugin(tauri_plugin_updater::Builder::new().build())
+    .plugin(tauri_plugin_autostart::init(
+      MacosLauncher::LaunchAgent,
+      Some(vec![]),
+    ))
+    .plugin(
+      tauri_plugin_log::Builder::new()
+        .timezone_strategy(TimezoneStrategy::UseLocal)
+        .targets([
+          Target::new(TargetKind::Stdout),
+          Target::new(TargetKind::LogDir { file_name: None }),
+          Target::new(TargetKind::Webview),
+        ])
+        .build(),
+    )
+    .plugin(tauri_plugin_process::init())
+    .plugin(tauri_plugin_os::init())
+    .plugin(tauri_plugin_dialog::init())
+    .plugin(tauri_plugin_clipboard_manager::init())
+    .plugin(tauri_plugin_store::Builder::default().build())
+    .plugin(tauri_plugin_opener::init())
+    .invoke_handler(tauri::generate_handler![
+      commands::openWin,
+      commands::importSetting,
+      commands::exportSetting,
+      commands::saveSetting,
+      commands::getSetting,
+      commands::clearStore,
+      commands::getHistoryOpenedUrls,
+      commands::clearHistoryOpenedUrls,
+      commands::killPort,
+      commands::getProjectNamesTree,
+      commands::openFolderEditor,
+      commands::hideDirWindow,
+      commands::setDirWindowSize,
+      commands::page_loaded,
+      commands::hideWindow,
+      commands::CopyAndPaste,
+      commands::updateQuickInputWindowSize,
+      commands::hideQuickInputWindow,
+      commands::get_package_info,
+      commands::checkUpdateRust,
+      commands::downloadAndInstall,
+      commands::saveCommands,
+      commands::getCommands,
+      commands::execCommand,
+      commands::check_update
+    ])
+    .on_window_event(|window, evt| match evt {
+      WindowEvent::CloseRequested { api, .. } => match window.label() {
+        "setting" | "openFolder" | "quickInput" => {
+          api.prevent_close();
+          window.hide();
+        }
+        _ => {}
+      },
+      WindowEvent::Focused(focused) => {
+        if (window.label() == "openFolder") {
+          if (!focused) {
+            // let closure = || println!("异步任务");
+            // let hand = tokio::spawn(async move {
+            //   sleep(Duration::from_millis(1000)).await;
+            //   closure();
+            // });
+
+            // window.hide();
+          }
+        }
+
+        let app = window.app_handle();
+        app.emit_to("openFolder", "focusChanged", focused).unwrap();
+      }
+      _ => {}
+    })
+    .on_webview_event(|window, event| {
+      dbg!(&event);
+    })
+    .on_page_load(|ww, payload| {
+      dbg!(&payload.event());
+    })
     .setup(|app| {
       #[cfg(desktop)]
       app
@@ -190,86 +270,6 @@ pub fn run() {
         app.global_shortcut().register(alt_r_shortcut);
       }
       return Ok(());
-    })
-    .plugin(tauri_plugin_autostart::init(
-      MacosLauncher::LaunchAgent,
-      Some(vec![]),
-    ))
-    .plugin(
-      tauri_plugin_log::Builder::new()
-        .timezone_strategy(TimezoneStrategy::UseLocal)
-        .targets([
-          Target::new(TargetKind::Stdout),
-          Target::new(TargetKind::LogDir { file_name: None }),
-          Target::new(TargetKind::Webview),
-        ])
-        .build(),
-    )
-    .plugin(tauri_plugin_process::init())
-    .plugin(tauri_plugin_os::init())
-    .plugin(tauri_plugin_updater::Builder::new().build())
-    .plugin(tauri_plugin_dialog::init())
-    .plugin(tauri_plugin_clipboard_manager::init())
-    .plugin(tauri_plugin_store::Builder::default().build())
-    .plugin(tauri_plugin_opener::init())
-    .invoke_handler(tauri::generate_handler![
-      commands::openWin,
-      commands::importSetting,
-      commands::exportSetting,
-      commands::saveSetting,
-      commands::getSetting,
-      commands::clearStore,
-      commands::getHistoryOpenedUrls,
-      commands::clearHistoryOpenedUrls,
-      commands::killPort,
-      commands::getProjectNamesTree,
-      commands::openFolderEditor,
-      commands::hideDirWindow,
-      commands::setDirWindowSize,
-      commands::page_loaded,
-      commands::hideWindow,
-      commands::CopyAndPaste,
-      commands::updateQuickInputWindowSize,
-      commands::hideQuickInputWindow,
-      commands::get_package_info,
-      commands::checkUpdateRust,
-      commands::downloadAndInstall,
-      commands::saveCommands,
-      commands::getCommands,
-      commands::execCommand,
-      commands::check_update
-    ])
-    .on_window_event(|window, evt| match evt {
-      WindowEvent::CloseRequested { api, .. } => match window.label() {
-        "setting" | "openFolder" | "quickInput" => {
-          api.prevent_close();
-          window.hide();
-        }
-        _ => {}
-      },
-      WindowEvent::Focused(focused) => {
-        if (window.label() == "openFolder") {
-          if (!focused) {
-            // let closure = || println!("异步任务");
-            // let hand = tokio::spawn(async move {
-            //   sleep(Duration::from_millis(1000)).await;
-            //   closure();
-            // });
-
-            // window.hide();
-          }
-        }
-
-        let app = window.app_handle();
-        app.emit_to("openFolder", "focusChanged", focused).unwrap();
-      }
-      _ => {}
-    })
-    .on_webview_event(|window, event| {
-      dbg!(&event);
-    })
-    .on_page_load(|ww, payload| {
-      dbg!(&payload.event());
     })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
